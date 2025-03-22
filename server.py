76d63d29da37e824e6f1a60b83a2308e2cfc2e06
Mattia Marsetti <marsigliomarsetti@gmail.com>
2025-03-22 15:21:55 -0400

    flask app working

--- server.py
+++ server.py
@@ -4,18 +4,23 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/emotionDetector', methods=['POST'])
-def detect_emotion():
-    data = request.get_json()
-    if not data or 'text' not in data:
-        return jsonify({'error': 'Missing text input'}), 400
-    
-    text_to_analyze = data['text']
-    result = emotion_detector(text_to_analyze)
-    return jsonify(result)
-
+@app.route('/emotionDetector', methods=['GET'])
+def emotion_detect():
+    text_to_analyze = request.args.get('textToAnalyze')
+    if text_to_analyze:
+        emotion=emotion_detector(text_to_analyze)  # Replace with your function
+        output='For the given statement, the system response is: /n' + '"anger": ' + str(emotion["anger"]) + ' "disgust": '  + str(emotion["disgust"]) + '" fear": ' + str(emotion["fear"]) + '" joy": ' + str(emotion["joy"]) + ' "sadness": ' + str(emotion["sadness"]) + '\n\t' + '. The dominant emotion is: ' + emotion["dominant_emotion"]
+         
+        return output
+        #return jsonify({'For the given statement, the system response is ': emotion})
+    else:
+        return jsonify({'error': 'No text provided'}), 400
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
